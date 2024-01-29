d40c1fb0a920703d8b05f29e56c2bc5d0d64e425
subha61 <shubhabrata@destiny.ws>
2024-01-30 00:22:05 +0530

    changed cart model

--- cart.js
+++ cart.js
@@ -4,7 +4,7 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const CartItemSchema = new mongoose.Schema(
   {
-    item: {
+    product_id: {
       type: ObjectId,
       ref: "Product",
       required: true,
