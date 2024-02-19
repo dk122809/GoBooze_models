ecf0019b3168e342a9a970af08d1f161599b20f3
development <development.geektech@gmail.com>
2024-02-19 11:15:11 +0530

    update product and add variants

--- product.js
+++ product.js
@@ -4,112 +4,160 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const productSchema = new mongoose.Schema(
   {
-    product_name: {
-      type: String,
-      required: true,
-    },
-    product_code: {
-      type: String,
-      required: false,
-    },
-    product_description: {
-      type: String,
-      required: false,
-    },
-    collection: {
-      type: ObjectId,
-      ref: "Category",
-      required: true,
-    },
-    item_id: {
-      type: ObjectId,
-      ref: "Item",
-      required: true,
-    },
-    sub_collection: {
-      type: ObjectId,
-      ref: "SubCollection",
-      required: false,
-    },
-    selling_price: {
-      type: Number,
-      required: true,
-    },
-    cost_price: {
-      type: Number,
-      required: true,
-    },
-    gross_profit: {
-      type: Number,
-      required: false,
-    },
-    quantity: {
-      type: Number,
-      required: true,
-    },
-    discount: {
-      type: Number,
-      required: false,
-      default: 0,
-    },
 
-    average_rating: {
-      type: Number,
-      required: false,
-      default: 0,
+    productName: {
+      type: String,
+      required: true,
     },
-    number_of_ratings: {
-      type: Number,
-      required: false,
-      default: 0,
+    category: {
+        type: ObjectId,
+        ref: "category",
+        required: true,
     },
-    number_of_reviews: {
-      type: Number,
-      required: false,
-      default: 0,
+    subCategory: {
+        type: ObjectId,
+        ref: "subCategory",
+        required: true
     },
-    number_of_times_ordered: {
-      type: Number,
-      required: false,
-      default: 0,
-    },
-    in_favourites: {
-      type: Number,
-      required: false,
-      default: 0,
-    },
-    in_customer_cart: {
-      type: Number,
-      required: false,
-      default: 0,
-    },
-    is_popular: {
-      type: Boolean,
-      required: false,
-      default: false,
+    brand: {
+      type: ObjectId,
+      ref: "brand",
+      required: true,
     },
     tags: {
-      type: [String],
-      required: false,
+      type: Array,
+      default: [],
     },
-
-    added_by: {
-      type: ObjectId,
-      ref: "Admin",
-      required: true,
-    },
-    updated_by: {
+    addedBy: {
       type: ObjectId,
       ref: "Admin",
       required: false,
     },
-
+    updatedBy: {
+      type: ObjectId,
+      ref: "Admin",
+      required: false,
+    },
+    variants: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "variants",
+        index: true,
+      },
+    ],
     status: {
       type: String,
       enum: ["active", "inactive"],
       default: "active",
       required: true,
-    },
+    }
+
+    // product_name: {
+    //   type: String,
+    //   required: true,
+    // },
+    // product_code: {
+    //   type: String,
+    //   required: false,
+    // },
+    // product_description: {
+    //   type: String,
+    //   required: false,
+    // },
+    // collection: {
+    //   type: ObjectId,
+    //   ref: "Category",
+    //   required: true,
+    // },
+    // item_id: {
+    //   type: ObjectId,
+    //   ref: "Item",
+    //   required: true,
+    // },
+    // sub_collection: {
+    //   type: ObjectId,
+    //   ref: "SubCollection",
+    //   required: false,
+    // },
+    // selling_price: {
+    //   type: Number,
+    //   required: true,
+    // },
+    // cost_price: {
+    //   type: Number,
+    //   required: true,
+    // },
+    // gross_profit: {
+    //   type: Number,
+    //   required: false,
+    // },
+    // quantity: {
+    //   type: Number,
+    //   required: true,
+    // },
+    // discount: {
+    //   type: Number,
+    //   required: false,
+    //   default: 0,
+    // },
+
+    // average_rating: {
+    //   type: Number,
+    //   required: false,
+    //   default: 0,
+    // },
+    // number_of_ratings: {
+    //   type: Number,
+    //   required: false,
+    //   default: 0,
+    // },
+    // number_of_reviews: {
+    //   type: Number,
+    //   required: false,
+    //   default: 0,
+    // },
+    // number_of_times_ordered: {
+    //   type: Number,
+    //   required: false,
+    //   default: 0,
+    // },
+    // in_favourites: {
+    //   type: Number,
+    //   required: false,
+    //   default: 0,
+    // },
+    // in_customer_cart: {
+    //   type: Number,
+    //   required: false,
+    //   default: 0,
+    // },
+    // is_popular: {
+    //   type: Boolean,
+    //   required: false,
+    //   default: false,
+    // },
+    // tags: {
+    //   type: [String],
+    //   required: false,
+    // },
+
+    // added_by: {
+    //   type: ObjectId,
+    //   ref: "Admin",
+    //   required: true,
+    // },
+    // updated_by: {
+    //   type: ObjectId,
+    //   ref: "Admin",
+    //   required: false,
+    // },
+
+    // status: {
+    //   type: String,
+    //   enum: ["active", "inactive"],
+    //   default: "active",
+    //   required: true,
+    // },
   },
   {
     suppressReservedKeysWarning: true,
---
+++ variant.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const productSchema = new mongoose.Schema(
+  {
+    variantName: {
+      type: String,
+      required: true,
+    },
+    variantCode: {
+      type: String,
+      required: false,
+    },
+    variantImage: {
+      type: String,
+      required: false,
+    },
+    sellingPrice: {
+      type: Number,
+      required: true,
+    },
+    purchasePrice: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    discount: {
+      type: Number,
+      required: true,
+    },
+    finalSellingPrice: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+      default: "active",
+      required: true,
+    },
+    addedBy: {
+        type: ObjectId,
+        ref: "Admin",
+        required: false,
+      },
+      updatedBy: {
+        type: ObjectId,
+        ref: "Admin",
+        required: false,
+      },
+  },
+  {
+    suppressReservedKeysWarning: true,
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("variant", productSchema, "variants");
