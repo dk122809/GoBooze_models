b582c3c2968c7a751b1a6c851af3f5b016e1641a
Daljit <daljit@geekinformatic.com>
2024-02-28 18:29:30 +0530

    stock models change

--- stock.js
+++ stock.js
@@ -19,44 +19,20 @@ const inventorySchema = new mongoose.Schema(
       ref: "category",
       required: true,
     },
-    subCategory_id: {
+    sub_category_id: {
       type: ObjectId,
       ref: "subCategory",
       required: true
     },
-    purchase_price: {
-      type: Number,
-      default: 0,
-      required:true
-    },
-    selling_price: {
-      type: Number,
-      default: 0,
-      required:true
-    },
-    final_selling_price:{
-      type: Number,
-      default: 0,
-    },
-    gst: {
-      type: Number,
-      default: 0,
-      required:true
-    },
     number_of_products: {
       type: Number,
-      default:0,
-      required:true
-    },
-    discount: {
-      type: Number,
       default: 0,
       required:true
     },
-    quantity: {
+    minimum_qty: {
       type: Number,
       default: 0,
-      required:false
+      required:true
     },
   },
   {
