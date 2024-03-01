42846bbe58c6776c35d03bc65a69dd8a877d79d3
Daljit <daljit@geekinformatic.com>
2024-03-01 18:30:14 +0530

    brand  requred false

--- brands.js
+++ brands.js
@@ -11,7 +11,7 @@ const brandSchema = new mongoose.Schema(
     },
     description: {
         type:String,
-        required:true
+        required:false
     },
     brandImage: {
         type:String,
