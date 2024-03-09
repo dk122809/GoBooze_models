701b1a02b7d6c50d40adae96bf4afb82dec6e756
Ravi Kandala <ravikandala7@gmail.com>
2024-03-09 16:48:54 +0530

    address modal change

--- address.js
+++ address.js
@@ -6,13 +6,27 @@ const addressSchema = new mongoose.Schema(
     addressFullName: { type: String, required: true },
     addressPhoneNumber: { type: String, required: true },
     locality: { type: String, required: false },
-    country: { type: String, required: true },
+    country: { type: String, required: false },
     addressPincode: { type: String, required: true },
-    state: { type: String, required: true },
-    city: { type: String, required: true },
+    state: { type: String, required: false },
+    city: { type: String, required: false },
     houseNo: { type: String, required: false },
     roadName: { type: String, required: false },
     select: { type: String, required: false },
+    coordinates: {
+      lat: {
+        type: Number,
+        required: false,
+      },
+      lag: {
+        type: Number,
+        required: false,
+      },
+    },
+    map_url: {
+      type: String,
+      required: false,
+    },
     status: {
       type: String,
       enum: ["active", "inactive"],
