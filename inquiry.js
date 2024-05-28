aa8f53c80b5cd14fda86cebfa9f3dd53f8b71310
Salman Zahid <salman@geekinformatic.com>
2024-05-28 15:50:56 +0530

    inquiry model added

---
+++ inquiry.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const inquirySchema = new mongoose.Schema(
+  {
+    first_name: {
+        type: String,
+        required: true,
+      },
+      last_name: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+      },
+      phone: {
+        type: String,
+        required: false,
+      },
+      message: {
+        type: String,
+        required: false
+      }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model(
+  "Inquiry",
+  inquirySchema,
+  "inquiry"
+);
