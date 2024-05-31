617692b20fe68cb976669fb0e6092c4e1afbceb1
Salman Zahid <salman@geekinformatic.com>
2024-05-31 15:20:57 +0530

    content_management schema

---
+++ content_management.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const contentManagementSchema = new mongoose.Schema(
+    {
+        device_type: {
+            type: String,
+            required: false,
+            // enum: ["web", "mobile" , "both"],
+        },
+        banner_type: {
+            type: String,
+            required: false,
+            enum: ["landing_page_banner", "festival_banner", "subscribe_banner", "none"],
+        },
+        title: {
+            type: String,
+            required: false,
+        },
+        image: {
+            type: String,
+            required: false,
+        },
+        theme_type: {
+            type: String,
+            required: false,
+            enum: ["light", "dark"]
+        },
+        content: {
+            type: String,
+            required: false,
+        },
+        updated_by: {
+            type: ObjectId,
+            ref: "Admin",
+            required: false,
+        },
+        status: {
+            type: String,
+            enum: ["active", "inactive"],
+            default: "active",
+            required: true,
+        },
+    },
+    {
+        suppressReservedKeysWarning: true,
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model("ContentManagement", contentManagementSchema, "content_managements");
